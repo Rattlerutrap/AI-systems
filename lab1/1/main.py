302a6fe5bcbe27137cc47c653a972e991bfe0163
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 02:37:44 +0300

    tic-tac-toe

--- lab1/1/main.py
+++ lab1/1/main.py
@@ -1,15 +1,39 @@
 from sklearn import datasets 
+import os
 from sklearn.naive_bayes import GaussianNB 
 
-class DataEl:
-    def __init__(self):
-        pass
+def toNums(field):
+    temp = []
+    for tile in field:
+        if (tile == 'x'):
+            temp.append(1)
+        elif (tile == 'o'):
+            temp.append(0)
+        else:
+            temp.append(-1)
+    return temp
 
-iris = datasets.load_iris() 
- 
+def toNumRes(res):
+    return 1 if res == 'positive\n' else 0
+
+
+data = []
+target = []
+
+with open('tic_tac_toe.txt', 'r', encoding='utf-8') as file:
+    line = file.readline()
+    while line:
+        line = line.split(',')
+        target.append(toNumRes(line.pop()))
+        data.append(toNums(line))
+        line = file.readline()
+
+
+
+
 gnb = GaussianNB() 
  
-gnb.fit(iris.data, iris.target) 
+gnb.fit(data, target) 
  
-y_pred = gnb.predict(iris.data) 
-print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred).sum())) 
+y_pred = gnb.predict(data) 
+print("Number of mislabeled points out of a total %d points : %d" % (len(data), (target != y_pred).sum())) 
