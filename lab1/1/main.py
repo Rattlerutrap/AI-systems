7bcbe62d2c768dab1d59bea5e92639d94e759c42
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 16:41:30 +0300

    shuffled data

--- lab1/1/main.py
+++ lab1/1/main.py
@@ -1,5 +1,6 @@
 from sklearn.naive_bayes import GaussianNB, CategoricalNB
 import matplotlib.pyplot as plt
+import random
 
 def toNums(field):
     temp = []
@@ -48,10 +49,6 @@ for i in range(1, len(data)):
     trainToTestingRatio.append(len(x_train)/len(x_test))
     accuracy.append((y_pred == y_test).sum()/len(y_test))
 
-    # print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
-
-# print(trainToTestingRatio)
-# print(accuracy)
 
 plt.figure(figsize=(10, 6))
 plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
@@ -60,3 +57,28 @@ plt.ylabel('Accuracy')
 plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
 plt.grid(True)
 plt.show()
+
+
+trainToTestingRatio = []
+accuracy = []
+
+for i in range(len(data)):
+    data[i].append(target[i])
+random.shuffle(data)
+target = []
+for i in data:
+    target.append(i[-1])
+
+
+for i in range(1, len(data)):
+    x_train = data[:i]
+    y_train = target[:i]
+
+    x_test = data[i:]
+    y_test = target[i:]
+
+    gnb.fit(x_train, y_train)
+    y_pred = gnb.predict(x_test) 
+
+    trainToTestingRatio.append(len(x_train)/len(x_test))
+    accuracy.append((y_pred == y_test).sum()/len(y_test))
