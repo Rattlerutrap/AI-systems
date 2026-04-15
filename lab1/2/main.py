ace195c194c313a3bd44910e28225409a31c7b42
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 19:20:44 +0300

    misunderstand task

--- lab1/2/main.py
+++ lab1/2/main.py
@@ -2,31 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+n_neg = 20
+n_pos = 80
+
+
+mean_neg = [10, 14]
+std_neg = 3
+
+
+mean_pos = [19, 16]
+std_pos = 4
+
+
 np.random.seed(42)
+X_neg = np.random.normal(mean_neg, std_neg, size=(n_neg, 2))
+X_pos = np.random.normal(mean_pos, std_pos, size=(n_pos, 2))
 
-
-mean = [10, 14]  
-std = 3          
-
-data = np.random.normal(mean, std, size=(100, 2))
-
-print(data)
-
-plt.figure(figsize=(12, 5))
-
-plt.subplot(1, 2, 1)
-plt.hist(data[:, 0], bins=10, alpha=0.7, color='blue', edgecolor='black')
-plt.xlabel('Значение')
-plt.ylabel('Частота')
-plt.title('Гистограмма X1')
-plt.grid(True, alpha=0.3)
-
-plt.subplot(1, 2, 2)
-plt.hist(data[:, 1], bins=10, alpha=0.7, color='green', edgecolor='black')
-plt.xlabel('Значение')
-plt.ylabel('Частота')
-plt.title('Гистограмма X2')
-plt.grid(True, alpha=0.3)
-
-plt.tight_layout()
-plt.show()
