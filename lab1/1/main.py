d53459a7e6e9cea2c079d129dcffc5d74eb0a3c9
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 18:36:10 +0300

    shuffled spam

--- lab1/1/main.py
+++ lab1/1/main.py
@@ -111,7 +111,6 @@ def Spam():
             data.append(list(map(float, line)))
             line = file.readline()
 
-    print(target)
     gnb = GaussianNB() 
 
     trainToTestingRatio = []
@@ -138,5 +137,42 @@ def Spam():
     plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
     plt.grid(True)
     plt.show()
+
+
+    gnb = GaussianNB() 
+
+    trainToTestingRatio = []
+    accuracy = []
+
+    for i in range(len(data)):
+        data[i].append(target[i])
+    random.shuffle(data)
+    target = []
+    for i in data:
+        target.append(i[-1])
+        i.pop()
+
+    for i in range(1, len(data)):
+        x_train = data[:i]
+        y_train = target[:i]
+
+        x_test = data[i:]
+        y_test = target[i:]
+
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test) 
+
+        trainToTestingRatio.append(len(x_train)/len(x_test))
+        accuracy.append((y_pred == y_test).sum()/len(y_test))
+        print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
+    plt.xlabel('Train/Test Ratio')
+    plt.ylabel('Accuracy')
+    plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
+    plt.grid(True)
+    plt.show()
+    
 
 Spam()
