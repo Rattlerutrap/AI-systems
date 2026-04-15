4f1abf441af135482117be40147912e0171c84c4
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 19:33:37 +0300

    not shuffled spam rm

--- lab1/1/main.py
+++ lab1/1/main.py
@@ -111,33 +111,7 @@ def Spam():
             data.append(list(map(float, line)))
             line = file.readline()
 
-    gnb = GaussianNB() 
-
-    trainToTestingRatio = []
-    accuracy = []
-
-    for i in range(1, len(data)):
-        x_train = data[:i]
-        y_train = target[:i]
-
-        x_test = data[i:]
-        y_test = target[i:]
-
-        gnb.fit(x_train, y_train)
-        y_pred = gnb.predict(x_test) 
-
-        trainToTestingRatio.append(len(x_train)/len(x_test))
-        accuracy.append((y_pred == y_test).sum()/len(y_test))
-        print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
-    plt.xlabel('Train/Test Ratio')
-    plt.ylabel('Accuracy')
-    plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
-    plt.grid(True)
-    plt.show()
-
+    gnb = GaussianNB()
 
 
     trainToTestingRatio = []
