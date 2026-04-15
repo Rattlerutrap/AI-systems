ddba090967f12eeb78c86f53e556655c08af724b
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-15 18:31:23 +0300

    notshuffled spam

--- lab1/1/main.py
+++ lab1/1/main.py
@@ -2,7 +2,7 @@ from sklearn.naive_bayes import GaussianNB, CategoricalNB
 import matplotlib.pyplot as plt
 import random
 
-def toNums(field):
+def TTTtoNums(field):
     temp = []
     for tile in field:
         if (tile == 'x'):
@@ -13,83 +13,130 @@ def toNums(field):
             temp.append(0)
     return temp
 
-def toNumRes(res):
+def TTTtoNumRes(res):
     return 1 if res == 'positive\n' else 0
 
+def TicTacToe():
+    data = []
+    target = []
 
-data = []
-target = []
-
-with open('tic_tac_toe.txt', 'r', encoding='utf-8') as file:
-    line = file.readline()
-    while line:
-        line = line.split(',')
-        target.append(toNumRes(line.pop()))
-        data.append(toNums(line))
+    with open('tic_tac_toe.txt', 'r', encoding='utf-8') as file:
         line = file.readline()
+        while line:
+            line = line.split(',')
+            target.append(TTTtoNumRes(line.pop()))
+            data.append(TTTtoNums(line))
+            line = file.readline()
 
 
 
 
-gnb = CategoricalNB(min_categories=3) 
+    gnb = CategoricalNB(min_categories=3) 
 
-trainToTestingRatio = []
-accuracy = []
+    trainToTestingRatio = []
+    accuracy = []
 
-for i in range(1, len(data)):
-    x_train = data[:i]
-    y_train = target[:i]
+    for i in range(1, len(data)):
+        x_train = data[:i]
+        y_train = target[:i]
 
-    x_test = data[i:]
-    y_test = target[i:]
+        x_test = data[i:]
+        y_test = target[i:]
 
-    gnb.fit(x_train, y_train)
-    y_pred = gnb.predict(x_test) 
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test) 
 
-    trainToTestingRatio.append(len(x_train)/len(x_test))
-    accuracy.append((y_pred == y_test).sum()/len(y_test))
-
-
-plt.figure(figsize=(10, 6))
-plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
-plt.xlabel('Train/Test Ratio')
-plt.ylabel('Accuracy')
-plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
-plt.grid(True)
-plt.show()
+        trainToTestingRatio.append(len(x_train)/len(x_test))
+        accuracy.append((y_pred == y_test).sum()/len(y_test))
 
 
-trainToTestingRatio = []
-accuracy = []
-
-for i in range(len(data)):
-    data[i].append(target[i])
-random.shuffle(data)
-target = []
-for i in data:
-    target.append(i[-1])
-    i.pop()
+    plt.figure(figsize=(10, 6))
+    plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
+    plt.xlabel('Train/Test Ratio')
+    plt.ylabel('Accuracy')
+    plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
+    plt.grid(True)
+    plt.show()
 
 
-for i in range(1, len(data)):
-    x_train = data[:i]
-    y_train = target[:i]
+    trainToTestingRatio = []
+    accuracy = []
 
-    x_test = data[i:]
-    y_test = target[i:]
-
-    gnb.fit(x_train, y_train)
-    y_pred = gnb.predict(x_test) 
-
-    trainToTestingRatio.append(len(x_train)/len(x_test))
-    accuracy.append((y_pred == y_test).sum()/len(y_test))
-    print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
+    for i in range(len(data)):
+        data[i].append(target[i])
+    random.shuffle(data)
+    target = []
+    for i in data:
+        target.append(i[-1])
+        i.pop()
 
 
-plt.figure(figsize=(10, 6))
-plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
-plt.xlabel('Train/Test Ratio')
-plt.ylabel('Accuracy')
-plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
-plt.grid(True)
-plt.show()
+    for i in range(1, len(data)):
+        x_train = data[:i]
+        y_train = target[:i]
+
+        x_test = data[i:]
+        y_test = target[i:]
+
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test) 
+
+        trainToTestingRatio.append(len(x_train)/len(x_test))
+        accuracy.append((y_pred == y_test).sum()/len(y_test))
+        print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
+
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
+    plt.xlabel('Train/Test Ratio')
+    plt.ylabel('Accuracy')
+    plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
+    plt.grid(True)
+    plt.show()
+    
+def spamToNumRes(res):
+    return 1 if res == '"nonspam"\n' else 0;
+
+def Spam():
+    target = []
+    data = []
+
+    with open('spam.csv', 'r', encoding='utf-8') as file:
+        file.readline()
+        line = file.readline()
+        while line:
+            line = line.split(',')
+            line.pop(0)
+            target.append(spamToNumRes(line.pop()))
+            data.append(list(map(float, line)))
+            line = file.readline()
+
+    print(target)
+    gnb = GaussianNB() 
+
+    trainToTestingRatio = []
+    accuracy = []
+
+    for i in range(1, len(data)):
+        x_train = data[:i]
+        y_train = target[:i]
+
+        x_test = data[i:]
+        y_test = target[i:]
+
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test) 
+
+        trainToTestingRatio.append(len(x_train)/len(x_test))
+        accuracy.append((y_pred == y_test).sum()/len(y_test))
+        print(f"Train size: {len(x_train)} Test size: {len(y_test)}, Correct: {(y_pred == y_test).sum()}, Accuracy: {(y_pred == y_test).sum()/len(y_test)}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(trainToTestingRatio, accuracy, 'b-', linewidth=2)
+    plt.xlabel('Train/Test Ratio')
+    plt.ylabel('Accuracy')
+    plt.title('Зависимость точности от соотношения обучающей и тестовой выборок')
+    plt.grid(True)
+    plt.show()
+
+Spam()
