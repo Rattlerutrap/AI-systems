9b9a770f699a2641ba41a2e4b5dd3a40e9382c4d
Rattlerutrap <dimaaupov2005@gmail.com>
2026-04-16 13:20:53 +0300

    plot1

--- lab1/3/main.py
+++ lab1/3/main.py
@@ -2,7 +2,17 @@ import csv
 import random
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
+
+def printPlot1(neighbors, accuracies):
+    plt.figure(figsize=(8, 6))
+    plt.plot(neighbors, accuracies, linewidth=2)
+    plt.xlabel('Количество соседей')
+    plt.ylabel('Точность')
+    plt.title('График зависимости ошибки классификации от количества ближайших соседей')
+    plt.grid(alpha=0.3)
+    plt.show()
 
 FILENAME = 'glass.csv'
 
@@ -14,6 +24,8 @@ with open(FILENAME, "r", newline="") as file:
     for row in reader:
         data.append(list(map(float, row[1:])))
 
+random.seed(42)
+
 random.shuffle(data)
 target = []
 for i in data:
@@ -24,7 +36,11 @@ scaler = StandardScaler()
 data_normalized = scaler.fit_transform(data)
 
 
+metrics = ['cosine', 'manhattan', 'euclidean']
 
+
+accuracies = []
+neighbors = []
 
 trainSize = 150
 for i in range(1, 30):
@@ -43,6 +59,11 @@ for i in range(1, 30):
     total = len(x_test)
     correct = (y_pred == y_test).sum()
     wrong = total - correct
+    accuracy = correct/total
 
-    print(f'total: {total}, correct: {correct}, wrong: {wrong}, accuracy: {correct/total}, neighboors: {i}')
+    accuracies.append(accuracy)
+    neighbors.append(i)
 
+    print(f'total: {total}, correct: {correct}, wrong: {wrong}, accuracy: {accuracy}, neighboors: {i}')
+
+printPlot1(neighbors, accuracies)
